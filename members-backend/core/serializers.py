91d52b91104c4cb78230ee10cd1105ee31387c8e
ricardo-amdb <ricardo@amdb.com.br>
2020-07-19 19:21:00 -0300

    First Commit

---
+++ members-backend/core/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Member
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = ['id', 'name', 'surname', 'phone', 'photo']
+
+
+class MemberSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = ['id', 'name']
+
